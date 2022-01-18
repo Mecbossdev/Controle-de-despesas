0f6179203ad0a2d389e8ee55d7d1eed13e5c7205
Jeferson Fernandes <jeferson.fernandesdasilva@hotmail.com>
2022-01-18 14:47:05 -0200

    Atualizando

---
+++ controle-de-despesas-master/controle-de-despesas-master/index.js
@@ -0,0 +1,10 @@
+const dummyTransactions = [
+  { id: 1, name:'Bolo de brigadeiro', amount: -20 },
+  { id: 2, name:'Salário', amount: 300 },
+  { id: 3, name:'Torta de frango', amount: -10 },
+  { id: 4, name:'Violão', amount: 150 }
+]
+
+const addTransactionIntoDOM = transaction => {
+  
+}
