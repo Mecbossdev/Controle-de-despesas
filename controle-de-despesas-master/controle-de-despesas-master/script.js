fece981cec2a140c0a66f2d343d6176848582253
Jeferson Fernandes <jeferson.fernandesdasilva@hotmail.com>
2022-01-20 12:54:30 -0200

    atualizando pesquisa

--- controle-de-despesas-master/controle-de-despesas-master/script.js
+++ controle-de-despesas-master/controle-de-despesas-master/script.js
@@ -1,4 +1,10 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
 const dummyTransactions = [
   { id: 1, name:'Bolo de brigadeiro', amount: -20 },
@@ -6,6 +12,11 @@ const dummyTransactions = [
   { id: 3, name:'Torta de frango', amount: -10 },
   { id: 4, name:'Violão', amount: 150 }
 ]
+
+const removeTransaction = ID => {
+  dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID)
+  init()
+}
 
 const addTransactionIntoDOM = transaction => {
   const operator = transaction.amount < 0 ? '-' : '+'
@@ -31,15 +42,45 @@ const updateBalanceValues = () => {
     .filter(value => value > 0)
     .reduce((accumulator, value) => accumulator + value, 0)
     .toFixed(2)
-  const expense = transactionsAmounts
+  const expense = Math.abs(transactionsAmounts
     .filter(value => value < 0)
-    .reduce((accumulator, value) => accumulator + value, 0)
+    .reduce((accumulator, value) => accumulator + value, 0))
     .toFixed(2)
-    console.log(expense)
+    
+  balanceDisplay.textContent = `R$ ${total}`
+  incomeDisplay.textContent = `R$ ${income}`
+  expenseDisplay.textContent = `R$ ${expense}`
 }
 const init = () => {
+  transactionsUl.innerHTML = ''
   dummyTransactions.forEach(addTransactionIntoDOM)
   updateBalanceValues()
 }
 
 init()
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const transactionName = inputTransactionName.value.trim()
+  const transactionAmount = inputTransactionAmount.value.trim()
+
+  if (transactionName === '' || transactionAmount === '') {
+    alert('Por favor, preencha o nome quanto o valor da transação')
+    return
+  }
+
+  const transaction = { 
+    id:generateID(), 
+    name: transactionName, 
+    amount: Number(transactionAmount) 
+  }
+
+  dummyTransactions.push(transaction)
+  init()
+
+  inputTransactionName.valeu = ''
+  inputTransactionAmount.value = ''
+})
