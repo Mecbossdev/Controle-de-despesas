e210d39a97782bcdcdbc688696e722b72b6d7397
Jeferson Fernandes <jeferson.fernandesdasilva@hotmail.com>
2022-01-20 19:15:56 -0200

    localStore

--- controle-de-despesas-master/controle-de-despesas-master/script.js
+++ controle-de-despesas-master/controle-de-despesas-master/script.js
@@ -6,15 +6,13 @@ const form = document.querySelector('#form')
 const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
-const dummyTransactions = [
-  { id: 1, name:'Bolo de brigadeiro', amount: -20 },
-  { id: 2, name:'Salário', amount: 300 },
-  { id: 3, name:'Torta de frango', amount: -10 },
-  { id: 4, name:'Violão', amount: 150 }
-]
-
+const localStoreTransactions = JSON.parse(localStorage
+  .getItem('transaction'))
+let transactions = localStore
+  .getItem('transactions') !== null ? localStorageTransactions : []
 const removeTransaction = ID => {
-  dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID)
+  transactions = transactions.filter(transaction => 
+    transaction.id !== ID)
   init()
 }
 
@@ -33,7 +31,7 @@ const addTransactionIntoDOM = transaction => {
 }
 
 const updateBalanceValues = () => {
-  const transactionsAmounts = dummyTransactions
+  const transactionsAmounts = transactions
     .map(transaction => transaction.amount)
   const total = transactionsAmounts
     .reduce((accumulator, transaction) => accumulator + transaction, 0 )
@@ -53,7 +51,7 @@ const updateBalanceValues = () => {
 }
 const init = () => {
   transactionsUl.innerHTML = ''
-  dummyTransactions.forEach(addTransactionIntoDOM)
+  transactions.forEach(addTransactionIntoDOM)
   updateBalanceValues()
 }
 
@@ -78,7 +76,7 @@ form.addEventListener('submit', event => {
     amount: Number(transactionAmount) 
   }
 
-  dummyTransactions.push(transaction)
+  transactions.push(transaction)
   init()
 
   inputTransactionName.valeu = ''
